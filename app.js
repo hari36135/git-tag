9c613a2090f4daf646b22289ae6b85c323c8e7d1
hari <hari36135@gmail.com>
2023-10-17 18:02:56 +0530

    changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.locals.musician = musician;
 
 // start server
 const server = app.listen(port, () => {
-  console.log("Server started on port number " + port);
+  console.log("Server started on port numbers " + port);
 });
 
 module.exports = server;
